79437cb5f2f417ca31cba489d1371a131c86b52c
Scott Milliorn <scottmilliorn@gmail.com>
2023-02-09 22:12:42 -0800

    Json (#8)
    * npm json
    
    * npm script
    
    * db.json
    
    * db.json
    
    * backend

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const task = of(TASKS);
-    return task;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
